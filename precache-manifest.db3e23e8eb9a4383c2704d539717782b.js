15d9f2778f7efa771d3285fcd6b54e9c0c390aa1
José María Martín Muñoz <josemaria.martin169@gmail.com>
2022-06-21 05:07:15 +0200

    Updates

---
+++ precache-manifest.db3e23e8eb9a4383c2704d539717782b.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = [
+  {
+    "revision": "1751d1de1111dca599a7",
+    "url": "/devflix-react/static/css/main.c2dddbf1.chunk.css"
+  },
+  {
+    "revision": "1751d1de1111dca599a7",
+    "url": "/devflix-react/static/js/main.1751d1de.chunk.js"
+  },
+  {
+    "revision": "546dafc18c5050b214b1",
+    "url": "/devflix-react/static/js/1.546dafc1.chunk.js"
+  },
+  {
+    "revision": "d16968100509f19d39e8",
+    "url": "/devflix-react/static/js/2.d1696810.chunk.js"
+  },
+  {
+    "revision": "1bb680df02f5f19a7f8a",
+    "url": "/devflix-react/static/js/runtime~main.1bb680df.js"
+  },
+  {
+    "revision": "13895efefeaad8218535db4273cf6031",
+    "url": "/devflix-react/static/media/devflix-logo.13895efe.png"
+  },
+  {
+    "revision": "672608a05e1eec1bbe92bb8243792264",
+    "url": "/devflix-react/static/media/myFontRegular.672608a0.otf"
+  },
+  {
+    "revision": "e6914f49919955163f88bcd37ac5bf64",
+    "url": "/devflix-react/static/media/myFontBold.e6914f49.otf"
+  },
+  {
+    "revision": "1735b8087a7da730af7720fa80f666af",
+    "url": "/devflix-react/static/media/myFontMedium.1735b808.otf"
+  },
+  {
+    "revision": "2f025ce372d9179de64400cd695c94b8",
+    "url": "/devflix-react/static/media/img (1).2f025ce3.jpg"
+  },
+  {
+    "revision": "5b067a0c4a19f33650ff545e20bb1ea3",
+    "url": "/devflix-react/static/media/img (2).5b067a0c.jpg"
+  },
+  {
+    "revision": "a0ae68b7c8341d9721997f3e4bacc4cc",
+    "url": "/devflix-react/static/media/img (3).a0ae68b7.jpg"
+  },
+  {
+    "revision": "7c6eba9463ff1cbbf313071ff04a5bdf",
+    "url": "/devflix-react/static/media/img (4).7c6eba94.jpg"
+  },
+  {
+    "revision": "efd5fc2423652accb0d6b5e61468660a",
+    "url": "/devflix-react/static/media/img (5).efd5fc24.jpg"
+  },
+  {
+    "revision": "9bbedbbaae8106884543b286eb96e68a",
+    "url": "/devflix-react/static/media/img (6).9bbedbba.jpg"
+  },
+  {
+    "revision": "d70ff43f33d2a4b83bb5c508a4c1c82d",
+    "url": "/devflix-react/static/media/img (7).d70ff43f.jpg"
+  },
+  {
+    "revision": "aeea5f3ecdc70dc6b227b162be4ec436",
+    "url": "/devflix-react/static/media/img (8).aeea5f3e.jpg"
+  },
+  {
+    "revision": "27b7f6f281bb7338c944b81bad1211e4",
+    "url": "/devflix-react/static/media/img (9).27b7f6f2.jpg"
+  },
+  {
+    "revision": "6a3e82a179263a8623bc47c208f57c60",
+    "url": "/devflix-react/static/media/img (10).6a3e82a1.jpg"
+  },
+  {
+    "revision": "4fe1be9acf0e5c9114c9fbaaf69a6911",
+    "url": "/devflix-react/static/media/img (11).4fe1be9a.jpg"
+  },
+  {
+    "revision": "1a31983a80e112a50709630b455ef90c",
+    "url": "/devflix-react/static/media/img (12).1a31983a.jpg"
+  },
+  {
+    "revision": "897dde8b592dd046fdca36ad0b986fcf",
+    "url": "/devflix-react/static/media/img (13).897dde8b.jpg"
+  },
+  {
+    "revision": "6e1e287da53614647b9b33cc480c7a26",
+    "url": "/devflix-react/static/media/img (14).6e1e287d.jpg"
+  },
+  {
+    "revision": "a784c68a95c0fbe2cbf6cf8767fd4acf",
+    "url": "/devflix-react/static/media/img (15).a784c68a.jpg"
+  },
+  {
+    "revision": "c7a1ed4c9212c6da1d2825090b415b4a",
+    "url": "/devflix-react/static/media/img (16).c7a1ed4c.jpg"
+  },
+  {
+    "revision": "1ad2ec7d0802a1de4839a63d453aa75b",
+    "url": "/devflix-react/static/media/img (17).1ad2ec7d.jpg"
+  },
+  {
+    "revision": "87a7025b01e38e71a63c1da97b02c79b",
+    "url": "/devflix-react/static/media/img (18).87a7025b.jpg"
+  },
+  {
+    "revision": "c3421a260f6292785a107a6368b8438d",
+    "url": "/devflix-react/static/media/img (19).c3421a26.jpg"
+  },
+  {
+    "revision": "f153d4927aed09e7024be9a556703982",
+    "url": "/devflix-react/static/media/img (20).f153d492.jpg"
+  },
+  {
+    "revision": "fc7ca9a7bed42d965a8d1f1b9c513d6a",
+    "url": "/devflix-react/static/media/img (21).fc7ca9a7.jpg"
+  },
+  {
+    "revision": "17da8a5fd248c15fcb6fdf67cab65c19",
+    "url": "/devflix-react/static/media/img (22).17da8a5f.jpg"
+  },
+  {
+    "revision": "75ee850dd94c10ef06510d8c591bb909",
+    "url": "/devflix-react/static/media/img (23).75ee850d.jpg"
+  },
+  {
+    "revision": "7f13c162903cce9adb678094d4776679",
+    "url": "/devflix-react/static/media/img (24).7f13c162.jpg"
+  },
+  {
+    "revision": "ce68daa678e5b1a1bcc3215b47386c7b",
+    "url": "/devflix-react/static/media/img (26).ce68daa6.jpg"
+  },
+  {
+    "revision": "455ac1662b9ec4176bc06def2c1559cf",
+    "url": "/devflix-react/static/media/img (27).455ac166.jpg"
+  },
+  {
+    "revision": "a0572e7fe8312e4793ff270b1cd69780",
+    "url": "/devflix-react/static/media/img (28).a0572e7f.jpg"
+  },
+  {
+    "revision": "106173dc8d9338ae43e996a42a020373",
+    "url": "/devflix-react/static/media/img (29).106173dc.jpg"
+  },
+  {
+    "revision": "1e5bc95f8f8c30338c6c37fb3c4954ac",
+    "url": "/devflix-react/static/media/img (30).1e5bc95f.jpg"
+  },
+  {
+    "revision": "f69e207bfb497c77bbc48d3f9472cac2",
+    "url": "/devflix-react/static/media/img (31).f69e207b.jpg"
+  },
+  {
+    "revision": "de720ae7502a10a9e73aaacb284314fe",
+    "url": "/devflix-react/static/media/img (32).de720ae7.jpg"
+  },
+  {
+    "revision": "ad64f140051051332d8f01bb91b9a888",
+    "url": "/devflix-react/static/media/img (33).ad64f140.jpg"
+  },
+  {
+    "revision": "43f176ec556b0dfeecbce78b7d4ff08a",
+    "url": "/devflix-react/static/media/img (34).43f176ec.jpg"
+  },
+  {
+    "revision": "99fa23b22140421e31ead51ff2c5e92b",
+    "url": "/devflix-react/static/media/img (35).99fa23b2.jpg"
+  },
+  {
+    "revision": "f0478e29285b50d0e6669959eb6ac2b0",
+    "url": "/devflix-react/static/media/img (36).f0478e29.jpg"
+  },
+  {
+    "revision": "e63f37de05830e85382ec61b50c47769",
+    "url": "/devflix-react/static/media/img (37).e63f37de.jpg"
+  },
+  {
+    "revision": "2a6bb524db7f550da2c9eb9730648310",
+    "url": "/devflix-react/static/media/img (38).2a6bb524.jpg"
+  },
+  {
+    "revision": "4d008c0c2b3b3c04873be0858418c19e",
+    "url": "/devflix-react/static/media/img (39).4d008c0c.jpg"
+  },
+  {
+    "revision": "1adc524ba1729846b72225aaafc51c0b",
+    "url": "/devflix-react/static/media/img (40).1adc524b.jpg"
+  },
+  {
+    "revision": "3299d4f35f5ca35328064e0fa896e408",
+    "url": "/devflix-react/static/media/img (41).3299d4f3.jpg"
+  },
+  {
+    "revision": "3b3618540c4cf8940ba2e3927b91829e",
+    "url": "/devflix-react/static/media/img (42).3b361854.jpg"
+  },
+  {
+    "revision": "413bb1756fd55c65f2b474e8c74e1c4d",
+    "url": "/devflix-react/static/media/img (43).413bb175.jpg"
+  },
+  {
+    "revision": "f51414c44f96a79fd969dea3a9916d6b",
+    "url": "/devflix-react/static/media/img (44).f51414c4.jpg"
+  },
+  {
+    "revision": "6b13d7c4beaa54c03dd11d8518a09864",
+    "url": "/devflix-react/static/media/img (45).6b13d7c4.jpg"
+  },
+  {
+    "revision": "b16b46ab4eeb211100b15aedf29e3e3d",
+    "url": "/devflix-react/static/media/img (46).b16b46ab.jpg"
+  },
+  {
+    "revision": "855ba945326bf6cdd6e58903923054fc",
+    "url": "/devflix-react/static/media/img (47).855ba945.jpg"
+  },
+  {
+    "revision": "931255e7dfaf86327b9d242ed8c95370",
+    "url": "/devflix-react/static/media/trailer.931255e7.mp4"
+  },
+  {
+    "revision": "6e4e14258defeacb265455be36b75622",
+    "url": "/devflix-react/static/media/placeholder.6e4e1425.jpg"
+  },
+  {
+    "revision": "b6d320fa05ac0e6035349e048ca55c6f",
+    "url": "/devflix-react/static/media/show-logo.b6d320fa.png"
+  },
+  {
+    "revision": "38e81bfe679e3d77f39c95b3a390a53a",
+    "url": "/devflix-react/index.html"
+  }
+];
